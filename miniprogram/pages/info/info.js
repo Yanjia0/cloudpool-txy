a84cfc91cc4861549645c0c61086259a6790bd92
unknown <unknown@example.com>
2019-12-19 23:30:04 +0800

    P16
    p16并没有学完我好菜

--- miniprogram/pages/info/info.js
+++ miniprogram/pages/info/info.js
@@ -1,4 +1,7 @@
 // miniprogram/pages/info/info.js
+const db=wx.cloud.database()
+const caramelCollection=db.collection('caramel')
+const _=db.command
 Page({
 
   /**
@@ -12,9 +15,44 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    caramelCollection.doc(options.id).field({
+      title:true,
+      price:true,
+      price:true,
+      tags:true
+    }).get({
+      success:res=>{
+        this.setData({
+          product:res.data,
+          id:options.id
+        })
+      }
+    })
   },
 
+  addtag:function(event){
+    caramelCollection.doc(this.data.id).update({
+      data:{
+        tags:_.push('cloud','pool')
+      }
+    }).then(res=>{console.log(res)})
+  },
+
+  deltag: function (event) {
+    caramelCollection.doc(this.data.id).update({
+      data: {
+        tags: _.shift()
+      }
+    }).then(res => { console.log(res) })
+  },
+
+update:function(event){
+  caramelCollection.doc(this.data.id).update({
+    data: {
+      title: "更新后的title"
+    }
+  })
+},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
